cd91fe1540566085dcfc8f8069817e5b0123e05e
cm-fujii-genki <fujii.genki@classmethod.jp>
2019-08-19 12:30:13 +0900

    update awscdk-circle_ci_deploy_sample.ts

--- bin/awscdk-circle_ci_deploy_sample.ts
+++ bin/awscdk-circle_ci_deploy_sample.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { AwscdkCircleCiDeploySampleStack } from '../lib/awscdk-circle_ci_deploy_sample-stack';
+import { AppStack } from '../lib/app-stack';
+import { CircleCIUserStack } from '../lib/circleci-user-stack';
+
+const targetEnv = process.env.SYSTEM_ENV ? process.env.SYSTEM_ENV : 'dev';
 
 const app = new cdk.App();
-new AwscdkCircleCiDeploySampleStack(app, 'AwscdkCircleCiDeploySampleStack');
+
+new AppStack(app, `AWS-CDK-CircleCI-Deploy-Sample-App-Stack-${targetEnv}`, targetEnv);
+
+new CircleCIUserStack(app, `AWS-CDK-CircleCI-Deploy-Sample-User-Stack-${targetEnv}`, targetEnv);
